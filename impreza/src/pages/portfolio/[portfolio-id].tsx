e7dde0ef76562c2c67baa0f7167204f6c3fd8305
Lucas Harada <lucasyharada@gmail.com>
2022-11-14 16:49:21 +0000

    move to src dir

---
+++ impreza/src/pages/portfolio/[portfolio-id].tsx
@@ -0,0 +1,6 @@
+export async function getStaticPaths() {
+    return {
+      paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+      fallback: false, // can also be true or 'blocking'
+    }
+  }
